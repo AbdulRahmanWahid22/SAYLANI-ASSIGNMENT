6404a761322d6bcaf9a900904388afb7ccc065bf
AbdulRahmanWahid22 <a.rahmanwahid25@gmail.com>
2024-01-08 07:40:18 -0800

    Update test.dart

--- test.dart
+++ test.dart
@@ -3,9 +3,9 @@ void main() {
   bool password = true;
   if (email == false && password == true) {
     print("Email incorrect");
-  } else if (email ==true && password==false) {
+  } else if (email == true && password == false) {
     print("password incorrect");
-  } else if(email==false && password==false){
+  } else if (email == false && password == false) {
     print("Email and password both are incorrect");
   } else {
     print("email and password both are correct");
